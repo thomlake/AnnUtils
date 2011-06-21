239d5bd30cded583227d6acde517fdb2d939ddbd
thom lake <thom.l.lake@gmail.com>
2011-06-21 12:05:52 -0400

    added some activation functions to functions.py

--- annutils/functions.py
+++ annutils/functions.py
@@ -1,6 +1,21 @@
-import numpy
+import numpy, random, math
 
 def set_diagonal(m, x = 0.):
 	m.flat[::m.shape[1]+1] = x
 	return m
+
+def _pthresh(x, t = 1.):
+	i = x*t
+	if i < -45:
+		return 0.
+	if i > 45:
+		return 1.
+	if 1./(1.+math.exp(-i)) < random.random():
+		return 0.
+	return 1.
+
+pthresh = numpy.vectorize(_pthresh)
+
+def sigmoid(a):
+	return 1./(1.+numpy.exp(-a))
 
